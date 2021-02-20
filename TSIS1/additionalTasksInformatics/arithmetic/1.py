40446c730f6d619d78d0d82cbfa71bc28675e1b0
Dimash <d_tleuzhanuly@kbtu.kz>
2021-02-21 00:49:17 +0600

    some game code

--- TSIS1/additionalTasksInformatics/arithmetic/1.py
+++ TSIS1/additionalTasksInformatics/arithmetic/1.py
@@ -1,3 +1,6 @@
-a = int(input())
-b = int(input())
-print((a**2 + b**2)**0.5)
+# a = int(input())
+# b = int(input())
+# print((a**2 + b**2)**0.5)
+
+if __name__ == '__main__':
+    li = [[1], [3], [4], [5]]
--- game_test/course_raywenderlich/main.py
+++ game_test/course_raywenderlich/main.py
@@ -1,5 +1,5 @@
 import pygame
-import math
+from math import atan2, cos , sin
 
 
 def draw_bg():
@@ -18,21 +18,30 @@ def quiting():
         if event.type is pygame.QUIT:
             pygame.quit()
             exit(0)
-
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            position = pygame.mouse.get_pos()
+            acc[1] += 1
+            arrows.append(
+                [atan2(position[1] - (playerpos1[1]), position[0] - (playerpos1[0])), playerpos1[0],
+                 playerpos1[1]])
+angle = 0
 
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption("Rabbit Game")
     width, height = 640, 480
-    x, y = 100, 100
+    positionPlayer = [100, 100]
     screen = pygame.display.set_mode((width, height))
     movement = 10
+    acc = [0, 0]
+    arrows = []
 
     # images
 
     player = pygame.image.load('resources/images/dude.png')
     grass = pygame.image.load('resources/images/grass.png')
     castle = pygame.image.load('resources/images/castle.png')
+    arrow = pygame.image.load('resources/images/bullet.png')
 
     while 1:
         pygame.time.Clock().tick(30)
@@ -43,23 +52,39 @@ if __name__ == '__main__':
         pressed = pygame.key.get_pressed()
 
         if pressed[pygame.K_w]:
-            y -= movement
+            positionPlayer[1] -= movement
         elif pressed[pygame.K_a]:
-            x -= movement
+            positionPlayer[0] -= movement
         elif pressed[pygame.K_s]:
-            y += movement
+            positionPlayer[1] += movement
         elif pressed[pygame.K_d]:
-            x += movement
-        #### this part i didnt understand yet
+            positionPlayer[0] += movement
+
         position = pygame.mouse.get_pos()
-        angle = math.atan2(position[1] - (y + 32), position[0] - (x + 26))
-        playerrot = pygame.transform.rotate(player, 360 - angle * 57.29)
-        playerpos1 = (x - playerrot.get_rect().width / 2, y - playerrot.get_rect().height / 2)
-        #####
+        angle = atan2(position[1] - (positionPlayer[1] + 23), position[0] - (positionPlayer[0] + 32)) * 57.32
+        playerrot = pygame.transform.rotate(player, 360 - angle)
+        playerpos1 = (positionPlayer[0] - playerrot.get_width() // 2, positionPlayer[1] - playerrot.get_height() // 2)
+
+        for event in pygame.event.get():
+            if event.type is pygame.MOUSEBUTTONDOWN:
+                posMouse = pygame.mouse.get_pos()
+                acc[1] += 1
+                arrows.append([atan2(posMouse[1] - (positionPlayer[1] + 23), posMouse[0] - (positionPlayer[0] + 32)), positionPlayer[0] + 32, positionPlayer[1] + 23])
+
+        for bullet in arrows:
+            index = 0
+            velx = cos(bullet[0])*movement
+            vely = sin(bullet[0])*movement
+            bullet[1] += velx
+            bullet[2] += vely
+            if bullet[1] <  -64 or bullet[1] > 640 or bullet[2] < -64 or bullet[2] > 480:
+                arrows.pop(index)
+            index += 1
+            for projectile in arrows:
+                arrowrot = pygame.transform.rotate(arrow, 360 - projectile[0] * 57.29)
+                screen.blit(arrowrot, (projectile[1], projectile[2]))
+
         screen.blit(playerrot, playerpos1)
 
 
         pygame.display.update()
-
-
-
