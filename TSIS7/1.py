f90d7a685eb28a5b8121ab82c7b4849567ca81c4
Dimash <d_tleuzhanuly@kbtu.kz>
2021-04-02 22:54:15 +0600

    try TSIS7

--- TSIS7/1.py
+++ TSIS7/1.py
@@ -2,51 +2,89 @@ import pygame, sys
 from math import cos, sin, pi
 
 
-class MyTable:
-    def __init__(self):
-        pass
+class GameObjects:
+    def __init__(self, x=100, y=50):
+        self.x = x
+        self.y = y
+        self.table_size = [display_size[0] - self.x * 2, display_size[1] - self.y * 2]
+
+
+class MyTable(GameObjects):
+    def __init__(self, x=60, y=50):
+        GameObjects.__init__(self, x, y)
 
     def draw(self):
-        x, y = 60, 10
-        lineWight = 2
-        table_size = [display_size[0] - 100, display_size[1] - 100]
+        for x in range(self.x, self.x + self.table_size[0] + 1, self.table_size[0] // 6):
+            pygame.draw.line(screen, BLACK, (x, self.y), (x, self.y + self.table_size[1]))
 
-        pygame.draw.rect(screen, BLACK, pygame.Rect(x, y, table_size[0], table_size[1]), lineWight)  # main border
-        pygame.draw.line(screen, BLACK, (table_size[0] // 2 + x, y), (table_size[0] // 2 + x, table_size[1] + y),
-                         lineWight)  # y-axis
-        pygame.draw.line(screen, BLACK, (x, table_size[1]//2 + y), (x + table_size[0], table_size[1]//2 + y), lineWight)# x-axis
-`
+        for y in range(self.y, self.y + self.table_size[1] + 1, self.table_size[1] // 8):
+            pygame.draw.line(screen, BLACK, (self.x, y), (self.x + self.table_size[0], y))
 
-class MyCos:
-    def __init__(self, points=[]):
+        pygame.draw.line(screen, BLACK, (self.x + self.table_size[0] // 2, self.y),
+                         (self.x + self.table_size[0] // 2, self.y + self.table_size[1]), 3)  # y = axis
+        pygame.draw.line(screen, BLACK, (self.x, self.y + self.table_size[1] // 2),
+                         (self.x + self.table_size[0], self.y + self.table_size[1] // 2), 3)  # X - axis
+
+
+class MyCos(GameObjects):
+    def __init__(self, x=60, y=50, points=None):
+
+        GameObjects.__init__(self, x, y)
+        if points is None:
+            points = []
+
+        self.points = points
+        self.k = (self.table_size[0]) / (6 * pi)
+        self.Amplitude = self.table_size[1] // 2
+        # getting points for cos
+        for x in range(self.table_size[0]):
+            y = int(cos(x / self.k) * self.Amplitude + display_size[1] // 2)
+            self.points.append([x + self.x, y])
+
+    def draw(self):
+        pygame.draw.aalines(screen, BLUE, False, self.points)
+
+
+class MySin(GameObjects):
+    def __init__(self, x=60, y=50, points=None):
+        GameObjects.__init__(self, x, y)
+        if points is None:
+            points = []
         self.points = points
 
-        for x in range(display_size[0]):
-            y = int(-cos(x / 840 * 6 * pi) * 200 + display_size[1] // 2)
-            self.points.append([x, y])
+        self.k = (self.table_size[0]) / (6 * pi)
+        self.Amplitude = self.table_size[1] // 2
+
+        # getting points for sin
+        for x in range(self.table_size[0]):
+            y = int(sin(x / self.k) * self.Amplitude + display_size[1] // 2)
+            self.points.append([x + self.x, y])
+        print(self.table_size)
 
     def draw(self):
-        pygame.draw.lines(screen, RED, False, self.points, 3)
+        pygame.draw.aalines(screen, RED, False, self.points)
 
 
-display_size = [840, 700]
+display_size = [1050, 700]
 screen = pygame.display.set_mode(display_size)
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
 graphObjects = []
 
 if __name__ == '__main__':
     pygame.init()
 
     myCos = MyCos()
+    mySin = MySin()
     myTable = MyTable()
-    graphObjects.extend([myTable,myCos])
+    graphObjects.extend([mySin, myCos, myTable])
 
     screen.fill(WHITE)
 
-    for object in graphObjects:
-        object.draw()
+    for obj in graphObjects:
+        obj.draw()
 
     while True:
         for event in pygame.event.get():
