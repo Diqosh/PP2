153d040932b39d2484b282efc21cebac1bbcb28f
Dimash <d_tleuzhanuly@kbtu.kz>
2021-04-16 00:56:47 +0600

    'tsis9'

--- TSIS9/Snake.py
+++ TSIS9/Snake.py
@@ -1,10 +1,16 @@
-import pygame, random
+import pygame, random, sys, json
 from pygame.math import Vector2
 from pygame import *
 
 pygame.init()
 
+json_file = open('Materials/snake.json', 'r', encoding='utf-8')
+myDict = json.load(json_file)
+counter_for_level = 0
+list_bodies_snake_after_pause = []
+direction_snake_after_pause = None
 
+PAUSE = False
 score = 0
 mouse_click = False
 eat_sound = pygame.mixer.Sound('Materials/eat.wav')
@@ -59,10 +65,16 @@ class Bacgraund:
 
 
 class Snake(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, body=None, direction=None):
         super(Snake, self).__init__()
-        self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
-        self.direction = Vector2(1, 0)
+        if body == None:
+            self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
+        else:
+            self.body = body
+        if direction == None:
+            self.direction = Vector2(1, 0)
+        else:
+            self.direction = direction
         self.needToAdd = False
 
     def draw(self):
@@ -95,15 +107,19 @@ class Snake(pygame.sprite.Sprite):
             eat_sound.play()
 
     def condiotions_game_over(self):
-        # if not 1 <= self.body[-1].x < numOfCeil - 1 or  not 1 <= self.body[-1].y < numOfCeil - 1:
-        #     self.game_over()
-        # for block in self.body[:-1]:
-        #     if block == self.body[-1]:
-        #         self.game_over()
+        if not 1 <= self.body[-1].x < numOfCeil - 1 or not 1 <= self.body[-1].y < numOfCeil - 1:
+            self.kill()
+            self.game_over()
+
+        for block in self.body[:-1]:
+            if block == self.body[-1]:
+                self.kill()
+                self.game_over()
         pass
 
     def game_over(self):
-        pygame.quit()
+
+        menu()
 
 
 class Friut(pygame.sprite.Sprite):
@@ -117,25 +133,23 @@ class Friut(pygame.sprite.Sprite):
         SCREEN.blit(apple_image, (self.x * sizeOfCeil, self.y * sizeOfCeil))
 
 
-
-
-
 fruit = Friut()
-snake = Snake()
+
 bg = Bacgraund()
 
-game_obj = []
-game_obj.extend([bg, fruit, snake])
 
-
-def game_loop(level):
+def game_loop(level, body_snake_after_pause, direction_snake):
+    global list_bodies_snake_after_pause, direction_snake_after_pause
+    snake = Snake(body_snake_after_pause, direction_snake)
+    game_obj = []
+    game_obj.extend([bg, fruit, snake])
     # every 100 ms rum snake.move
+    global PAUSE
     level += 1
     ms = 120 // level
     SCREEN_UPDATE = pygame.USEREVENT
     pygame.time.set_timer(SCREEN_UPDATE, ms)
-
-    while True:
+    while not PAUSE:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -155,6 +169,10 @@ def game_loop(level):
                 elif event.key == pygame.K_s:
                     if snake.direction.y != -1:
                         snake.direction = Vector2(0, 1)
+                elif event.key == K_ESCAPE:
+                    PAUSE = True
+                    list_bodies_snake_after_pause = snake.body
+                    direction_snake_after_pause = snake.direction
 
         for obj in game_obj:
             obj.draw()
@@ -164,46 +182,55 @@ def game_loop(level):
 
 
 def menu():
-    global mouse_click
-    counter_for_level = 0
-    level = ['Easy', 'Medium', 'dont try please']
+    global mouse_click, PAUSE, list_bodies_snake_after_pause, direction_snake_after_pause, counter_for_level, myDict
 
+    level = ['Easy', 'Medium', 'even dont try please']
 
     while True:
+
         # counter handler
         if counter_for_level > 2:
             counter_for_level = 0
 
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
+                sys.exit()
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
                     mouse_click = True
 
         SCREEN.fill(BLACK)
-        rect_start = draw_text('start game', WHITE, DISPLAY_SIZE[0]//2 , 200)
+        rect_start = draw_text('start game', WHITE, DISPLAY_SIZE[0] // 2, 200)
         rect_level = draw_text(f'level: {level[counter_for_level]}', WHITE, DISPLAY_SIZE[0] // 2, 300)
+        rect_resume = draw_text('resume', WHITE, DISPLAY_SIZE[0] // 2, 400)
+        rect_save = draw_text('save', WHITE, DISPLAY_SIZE[0] // 2, 500)
+        rect_quit = draw_text('quit', WHITE, DISPLAY_SIZE[0] // 2, 600)
         x_pos_mouse, y_pos_mouse = pygame.mouse.get_pos()
 
         if rect_start.collidepoint((x_pos_mouse, y_pos_mouse)):
             draw_text('start game', RED, DISPLAY_SIZE[0] // 2, 200)
             if mouse_click:
                 mouse_click = False
-                game_loop(counter_for_level)
+                game_loop(counter_for_level, None, None)
+
         if rect_level.collidepoint((x_pos_mouse, y_pos_mouse)):
             draw_text(f'level: {level[counter_for_level]}', RED, DISPLAY_SIZE[0] // 2, 300)
             if mouse_click:
                 mouse_click = False
-
                 counter_for_level += 1
-
-
-
-
-
-
+        if rect_resume.collidepoint((x_pos_mouse, y_pos_mouse)):
+            draw_text('resume', RED, DISPLAY_SIZE[0] // 2, 400)
+            if mouse_click:
+                PAUSE = False
+                game_loop(counter_for_level, list_bodies_snake_after_pause, direction_snake_after_pause)
+        if rect_save.collidepoint((x_pos_mouse, y_pos_mouse)):
+            draw_text('save', RED, DISPLAY_SIZE[0] // 2, 500)
+            if mouse_click:
+                pass
+        if rect_quit.collidepoint((x_pos_mouse, y_pos_mouse)):
+            draw_text('quit', RED, DISPLAY_SIZE[0] // 2, 600)
+            if mouse_click:
+                sys.exit()
 
         mouse_click = False
         pygame.display.update()
