87af31618f3b9d1ab4fc95204e4ad942d27ba6a8
Dimash <d_tleuzhanuly@kbtu.kz>
2021-04-15 22:40:25 +0600

    'tsis9'

--- TSIS9/Snake.py
+++ TSIS9/Snake.py
@@ -1,9 +1,14 @@
 import pygame, random
 from pygame.math import Vector2
+from pygame import *
 
 pygame.init()
+
+
 score = 0
+mouse_click = False
 eat_sound = pygame.mixer.Sound('Materials/eat.wav')
+eat_sound.set_volume(0.1)
 font = pygame.font.SysFont('roboto', 60)
 font_small = pygame.font.SysFont('roboto', 20)
 bg_block_image = pygame.image.load('Materials/bg.png')
@@ -13,6 +18,7 @@ table_image = pygame.image.load('Materials/table.png')
 table_image.set_alpha(200)
 sizeOfCeil = 40
 numOfCeil = 20
+DISPLAY_SIZE = [800, 800]
 clock = pygame.time.Clock()
 SCREEN = pygame.display.set_mode((sizeOfCeil * numOfCeil, sizeOfCeil * numOfCeil))
 pygame.display.set_caption('SNAKE')
@@ -20,6 +26,13 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
+
+
+def draw_text(text, color, xpos, ypos):
+    textobj = font.render(text, True, color)
+    text_rect = textobj.get_rect(center=(xpos, ypos))
+    SCREEN.blit(textobj, text_rect)
+    return text_rect
 
 
 class Bacgraund:
@@ -40,9 +53,10 @@ class Bacgraund:
             SCREEN.blit(wall_block_image, (0, y * sizeOfCeil))
             SCREEN.blit(wall_block_image, (sizeOfCeil * (numOfCeil - 1), y * sizeOfCeil))
         # table and score draw
-        SCREEN.blit(table_image,table_image.get_rect(topright=(numOfCeil * sizeOfCeil, 0)))
-        score_image = font_small.render(f'Score: {score}',True, BLACK)
-        SCREEN.blit(score_image,score_image.get_rect(topleft=((numOfCeil-3) * sizeOfCeil, 0)))
+        SCREEN.blit(table_image, table_image.get_rect(topright=(numOfCeil * sizeOfCeil, 0)))
+        score_image = font_small.render(f'Score: {score}', True, BLACK)
+        SCREEN.blit(score_image, score_image.get_rect(topleft=((numOfCeil - 3) * sizeOfCeil, 0)))
+
 
 class Snake(pygame.sprite.Sprite):
     def __init__(self):
@@ -103,9 +117,8 @@ class Friut(pygame.sprite.Sprite):
         SCREEN.blit(apple_image, (self.x * sizeOfCeil, self.y * sizeOfCeil))
 
 
-# every 100 ms rum snake.move
-SCREEN_UPDATE = pygame.USEREVENT
-pygame.time.set_timer(SCREEN_UPDATE, 120)
+
+
 
 fruit = Friut()
 snake = Snake()
@@ -113,7 +126,15 @@ bg = Bacgraund()
 
 game_obj = []
 game_obj.extend([bg, fruit, snake])
-if __name__ == '__main__':
+
+
+def game_loop(level):
+    # every 100 ms rum snake.move
+    level += 1
+    ms = 120 // level
+    SCREEN_UPDATE = pygame.USEREVENT
+    pygame.time.set_timer(SCREEN_UPDATE, ms)
+
     while True:
 
         for event in pygame.event.get():
@@ -135,10 +156,57 @@ if __name__ == '__main__':
                     if snake.direction.y != -1:
                         snake.direction = Vector2(0, 1)
 
-        SCREEN.fill(WHITE)
-
         for obj in game_obj:
             obj.draw()
 
         pygame.display.flip()
         clock.tick(60)
+
+
+def menu():
+    global mouse_click
+    counter_for_level = 0
+    level = ['Easy', 'Medium', 'dont try please']
+
+
+    while True:
+        # counter handler
+        if counter_for_level > 2:
+            counter_for_level = 0
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    mouse_click = True
+
+        SCREEN.fill(BLACK)
+        rect_start = draw_text('start game', WHITE, DISPLAY_SIZE[0]//2 , 200)
+        rect_level = draw_text(f'level: {level[counter_for_level]}', WHITE, DISPLAY_SIZE[0] // 2, 300)
+        x_pos_mouse, y_pos_mouse = pygame.mouse.get_pos()
+
+        if rect_start.collidepoint((x_pos_mouse, y_pos_mouse)):
+            draw_text('start game', RED, DISPLAY_SIZE[0] // 2, 200)
+            if mouse_click:
+                mouse_click = False
+                game_loop(counter_for_level)
+        if rect_level.collidepoint((x_pos_mouse, y_pos_mouse)):
+            draw_text(f'level: {level[counter_for_level]}', RED, DISPLAY_SIZE[0] // 2, 300)
+            if mouse_click:
+                mouse_click = False
+
+                counter_for_level += 1
+
+
+
+
+
+
+
+        mouse_click = False
+        pygame.display.update()
+
+
+menu()
